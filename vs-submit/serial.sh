e958453dad6aa71d2be95a349a35d957ab923f3e
Ubuntu <ubuntu@spark-master.node.dc1.consul>
2017-11-24 14:24:23 +0000

    serial experiments

--- vs-submit/serial.sh
+++ vs-submit/serial.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-docker run -v $PWD:/host --name docking_full mcapuccini/oe-docking \
+docker run -v $PWD/data:/host --name docking_full mcapuccini/oe-docking \
   fred -receptor /var/openeye/hiv1_protease.oeb \
   -hitlist_size 0 \
   -conftest none \
   -dbase /host/aksci_p0.0.sdf /host/aksci_p1.0.sdf \
   -docked_molecule_file /host/output.sdf
 
-docker run -v $PWD:/host --name sorting_full mcapuccini/sdsorter \
+docker run -v $PWD/data:/host --name sorting_full mcapuccini/sdsorter \
   sdsorter -reversesort='FRED Chemgauss4 score' \
   -keep-tag='FRED Chemgauss4 score' \
   -nbest=3 \
